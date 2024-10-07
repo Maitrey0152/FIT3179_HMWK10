5879897e13f0b2fc2185a7449bb2fe6d05a1b869
Maitrey0152 <msha0152@student.monash.edu>
2024-10-07 20:05:00 +1100

    Final submission for week 10 homework

---
+++ vis.js
@@ -0,0 +1,9 @@
+var vs1 = "map.json";
+vegaEmbed("#map",vs1).then(function(result){
+
+}).catch(console.error);
+
+var vs2 = "bar.json";
+vegaEmbed("#bar",vs2).then(function(result){
+
+}).catch(console.error);
